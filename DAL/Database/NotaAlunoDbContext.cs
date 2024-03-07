0709be50c3d7095eb3c4c4e20fd784f752c8cec2
celsonfernand0 <celsonfernando100@outlook.com>
2024-03-07 21:55:49 +0100

    git ignore teste 3

--- DAL/Database/NotaAlunoDbContext.cs
+++ DAL/Database/NotaAlunoDbContext.cs
@@ -29,7 +29,7 @@ namespace Api_Notas_Aluno.DAL.Database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // Configura as opções de conexão com o PostgreSQL
-            optionsBuilder.UseNpgsql("");
+            optionsBuilder.UseNpgsql("f");
         }
     }
 }
